68f3e6187aa9c57ea86f165cc09f902222d8a392
worldparliment <shychain22007@gmail.com>
2025-04-02 16:24:02 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # ATTEND-EASE
 ![Red Text](https://img.shields.io/badge/An%20Easy%20To%20Use%20Attendance%20System-red)
 
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function WEAREREADYTOTWORK() {
+  return (
+    <div>WEAREREADYTOTWORK</div>
+  )
+}
